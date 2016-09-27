e798f6426567b1868235bb3bd79cf1acee8db0aa
Reynaldo Cordero (naldoco) <reynaldo.cordero@gmail.com>
2016-09-27 23:45:41 +0200

    add toFrames. Green

--- src/Bowling.hs
+++ src/Bowling.hs
@@ -6,6 +6,7 @@ pins_1 = [1,4,4,5,6,4,5,5,10,0,1,7,3,6,4,10,2,8,6]    -- Total score: 133
 data Frame = Open   Int Int
            | Spare  Int Int
            | Strike Int Int
+           deriving (Eq, Show)
 
 bowling :: [Int] -> Int
 bowling pins
