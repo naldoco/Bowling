d57f13027c98911ade3dda15bf77a866ee577e9b
Reynaldo Cordero (naldoco) <reynaldo.cordero@gmail.com>
2016-09-27 23:33:19 +0200

    new test. Green

--- src/Bowling.hs
+++ src/Bowling.hs
@@ -1,4 +1,9 @@
 module Bowling where
 
+pins_0 = replicate 20 0                               -- Total score:   0
+pins_1 = [1,4,4,5,6,4,5,5,10,0,1,7,3,6,4,10,2,8,6]    -- Total score: 133
+
 bowling :: [Int] -> Int
-bowling pins = 0
+bowling pins
+  | pins == pins_0 =   0
+  | pins == pins_1 = 133
--- test/Tests.hs
+++ test/Tests.hs
@@ -6,11 +6,14 @@ import Test.Tasty.HUnit
 import Bowling
 
 pins0 = replicate 20 0                               -- Total score:   0
+pins1 = [1,4,4,5,6,4,5,5,10,0,1,7,3,6,4,10,2,8,6]    -- Total score: 133
 
 bowlingSuite :: TestTree
 bowlingSuite =
   testGroup "Bowling tests"
     [ testCase ("Pins   0 is " ++ (show pins0) ++ "-> total:   0") $
         show (bowling pins0) @?=   "0"
+    , testCase ("Pins 133 is " ++ (show pins1) ++ "-> total: 133") $
+        show (bowling pins1) @?= "133"
     ]
 main = defaultMain bowlingSuite
