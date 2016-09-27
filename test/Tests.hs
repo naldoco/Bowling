4558b412dfa8be7ff4e086677a59850444860831
Reynaldo Cordero (naldoco) <reynaldo.cordero@gmail.com>
2016-09-27 23:53:32 +0200

    several (Open n m) more. Red

--- test/Tests.hs
+++ test/Tests.hs
@@ -21,6 +21,27 @@ bowlingSuite =
       [ testCase "Zeros are Open 0 0s" $
           toFrames (replicate 20 0) @?= replicate 10 (Open 0 0)
       ]
+    , testGroup "toFrames" $
+        map (\(label, input, expected) ->
+                testCase label $ toFrames input @?= expected) tests
     ]
+
+tests :: [(String, [Int], [Frame])]    -- (label, pins, frames)
+tests =
+  [ ( "Zeros are Open 0 0"
+    , replicate 20 0
+    , replicate 10 (Open 0 0)
+    )
+  ,
+    ( "Ones  are Open 1 1"
+    , replicate 20 1
+    , replicate 10 (Open 1 1)
+    )
+  ,
+    ( "4+5   are Open 4 5"
+    , take 20 $ cycle [4,5]
+    , replicate 10 (Open 4 5)
+    )
+  ]
 
 main = defaultMain bowlingSuite
